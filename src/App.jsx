0460f2a4e6fa3cf108eaa377e372b139957d4585
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-01-06 03:52:03 -0500

    navbar darkmode theme

--- src/App.jsx
+++ src/App.jsx
@@ -9,34 +9,18 @@ import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import BrowseProducts from "./pages/BrowseProducts";
 import Home from "./pages/Home";
-import { useState } from "react";
+
 
 function App() {
-  const [mode, setMode] = useState('light');
-  const [text, setText] = useState ('Turn Dark Mode')
-
-  const handleToggle = () =>{
-    console.log('clicked')
-
-    if(mode === 'dark'){
-      setMode('light')
-      setText ('Turn Dark Mode')
-    }
-    else{
-      setMode('dark')
-      setText ('Turn Light Mode')
-    }
-  }
-
 
 
   return (
    
       <Router>
-        <Navbar mode = {mode} handleToggle = {handleToggle} text = {text}/>
+        <Navbar />
         <div>
         <Routes>
-          <Route path ="/" element={<Home mode = {mode} handleToggle = {handleToggle} text = {text}/>}/>
+          <Route path ="/" element={<Home />}/>
           <Route path ="/services" element={<BrowseProducts/>}/>
           <Route path ="/about" element={<About/>}/>
         </Routes>
