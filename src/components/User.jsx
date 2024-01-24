d0244671a6b285775e825b6032d7f7ac7a635e3f
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-01-24 12:19:01 -0500

    increase pic size

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,7 +4,7 @@ export default function User(props) {
   return (
     <div className="flex">
       <div className="flex flex-col">
-        <img className="w-40" src={props.userImage} />
+        <img className="w-60" src={props.userImage} />
         <div>user id: {props.userId} </div>
         <h3>{props.user}</h3>
       </div>
