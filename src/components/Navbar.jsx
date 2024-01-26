d5ce6b354eefa6bdcaa31ba1448994bcbf938afd
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-01-26 03:29:28 -0500

    add sign in form, fix navba and css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const handleExpandSubMenu = () =>{
 }
 
   return (
-    <nav className="sticky top-0 z-50 bg-gray-300 border-b border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+    <nav className="sticky top-0 z-50  dark:bg-gray-900 dark:border-gray-700 bg-gray-300 border-b border-gray-200">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-5">
   
 {/* <nav className = "fixed top-0 z-50 bg-gray-300 border-gray-200 dark:bg-gray-900 dark:border-gray-700">
@@ -39,7 +39,7 @@ const handleExpandSubMenu = () =>{
 
  <DarkModeToggle/>
     <button  onClick = {handleExpandMenu} data-collapse-toggle="navbar-dropdown" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
+        {/* <span className="sr-only">Open main menu</span> */}
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
