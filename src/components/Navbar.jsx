48c03c25a39e302459be8e49bf8c6d33b3bfdbbb
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-01-27 02:10:57 -0500

    navbar and home component work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const handleExpandSubMenu = () =>{
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
-    <div className={`${expand} w-full md:block md:w-auto sm:fixed sm:mt-72 md:mt-0 md:ml-48`} id="navbar-dropdown">
+    <div className={`${expand} w-full md:block md:w-auto sm:fixed sm:mt-64 md:mt-0 md:ml-48`} id="navbar-dropdown">
       <ul className="flex flex-col overflow-hidden font-medium p-4 md:p-0 mt-4 border border-gray-300 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-gray-300 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-900">
         <li>
           <Link  to="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Home</Link>
@@ -61,8 +61,8 @@ const handleExpandSubMenu = () =>{
     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
   </svg></button>
             {/* <!-- Dropdown menu --> */}
-            <div id="dropdownNavbar" className={`z-10 ${subExpand} fixed md:ml-[-60px] lg:mt-5 mt-4 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}>
-                <ul className="flex flex-wrap  mx-0 pr-5 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+            <div id="dropdownNavbar" className={`z-10 ${subExpand} fixed md:ml-[-128] lg:mt-5 mt-4 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-800 dark:divide-gray-800`}>
+                <ul className="flex flex-wrap  mx-0 pr-5 text-sm text-gray-900 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
                   <li>
                   <Link to="/about"className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">About</Link>
                   </li>
