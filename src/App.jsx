8e8249258c6fd49b132427fd8227a3dfd0cc79c0
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-02-10 23:02:21 -0500

    create signin user request

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,26 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import UserList from "./pages/UserList";
 import Home from "./pages/Home";
 import Register from "./pages/RegisterPage";
-
+import UserProfile from "./pages/UserProfile";
 
 function App() {
-
-
   return (
-   
-      <Router>
-        <Navbar />
-        <div>
+    <Router>
+      <Navbar />
+      <div>
         <Routes>
-          <Route path ="/" element={<Home />}/>
-          <Route path ="/userlist" element={<UserList/>}/>
-          <Route path ="/about" element={<About/>}/>
-          <Route path ="/register" element={<Register/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/userlist" element={<UserList />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/userprofile" element={<UserProfile />} />
         </Routes>
-        </div>
-     
-      </Router>
-      
-   
+      </div>
+    </Router>
   );
 }
 
