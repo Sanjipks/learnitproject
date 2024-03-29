f041e61d92232c14b9dee737c2acb310c5c9459a
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-03-29 11:22:31 -0400

    rmv comments

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -3,38 +3,16 @@ import { useEffect, useState } from "react";
 import User from "../components/User";
 import { getUsers } from "../apis/Api";
 
-
-
 export default function UserList() {
   const [userlist, setUserlist] = useState([]);
 
-  // async function getUsers() {
-  //   const getusers = await fetch("https://api.github.com/users");
-  //   const data = await getusers.json();
-  //   setUserlist(data);
-  // }
-
-  
-
-  // useEffect(() => {
-  //   // If you are using then method, use catch function to catch the error
-  //   getUsers()
-  //     .then((data) => setUserlist(data))
-      
-  // }, [])
-
-
-try {
-  useEffect( () => {
-    getUsers().then(setUserlist);
-  }, []
-  )
-} 
-catch (error) {
-  throw new Error('Error:', error);
-}
-
-  
+  try {
+    useEffect(() => {
+      getUsers().then(setUserlist);
+    }, []);
+  } catch (error) {
+    throw new Error("Error:", error);
+  }
 
   return (
     <div className="flex flex-col justify-items-center  bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
