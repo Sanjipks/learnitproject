f7e4432d11be080623c8a3d3eccf68880f59cf11
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-04-11 09:36:29 -0400

    some css values replaced

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -36,7 +36,7 @@ const Auth = () => {
 
                 <button
                   type="submit"
-                  className="w-full text-dark bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:text-white"
+                  className="w-full text-gray-900 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:text-white"
                 >
                   Verify
                 </button>
