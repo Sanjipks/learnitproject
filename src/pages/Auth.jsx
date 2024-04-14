1f15fcf0457f9b2b19a0374fe08d161b2f4f2bcb
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-04-13 23:11:35 -0400

    ..

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -9,7 +9,7 @@ const Auth = () => {
       <section className=" bg-gray-300 dark:bg-gray-600">
         <div className=" flex flex-col items-center justify-center md:h-dvh px-6 mx-auto lg:pt-6 ">
           <p className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            Verify Your Account
+            Authenticate Your Account
           </p>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
