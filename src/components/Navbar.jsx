5aaa81ad722c06c7f95b6a7e1429b029510df411
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-05-09 13:43:49 -0400

    minor fix in menuItem to show home page as landing page for user

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -72,10 +72,10 @@ export default function Navbar() {
           id="navbar-dropdown"
         >
           <ul className="flex flex-col overflow-hidden font-medium p-4 md:p-0 mt-4 border border-gray-300 sm:rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-900">
-            {localStorage.getItem("loginState") === null ? (
+            {localStorage.getItem("loginState") === "true" ? (
               <li>
                 <Link
-                  to="/"
+                  to="/home"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Home
@@ -103,10 +103,10 @@ export default function Navbar() {
             </li>
             <li>
               <Link
-                to="/"
+                to="/contactUs"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Contact
+                Contact Us
               </Link>
             </li>
             <li className="static">
