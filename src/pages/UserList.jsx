1a788653b781bd8180e76348173c1c767c20ea0f
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-05-31 02:26:24 -0400

    height fix

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -42,7 +42,7 @@ export default function UserList() {
   };
 
   return (
-    <div className="h-dvh sm:h-auto flex flex-col justify-items-center bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
+    <div className="min-h-dvh h-auto flex flex-col justify-items-center bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
       <h1 className="m-10 text-center text-xl">USERS</h1>
       <div className="flex justify-self-center mx-auto">
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
