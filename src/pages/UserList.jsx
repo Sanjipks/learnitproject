6ae0c83fcc8bcc2a3036250b368f6c1077a4118d
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-05-31 02:21:24 -0400

    ...

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -42,10 +42,10 @@ export default function UserList() {
   };
 
   return (
-    <div className="flex flex-col justify-items-center  bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
+    <div className="h-dvh sm:h-auto flex flex-col justify-items-center bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
       <h1 className="m-10 text-center text-xl">USERS</h1>
-      <div className="container mx-auto p-4">
-        <ul className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="flex justify-self-center mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {userlist.map((user, id) => (
             <div key={id} className="p-2">
               <User
@@ -57,7 +57,7 @@ export default function UserList() {
               />
             </div>
           ))}
-        </ul>
+        </div>
       </div>
     </div>
   );
