b6e73f6a9399ef0a141be706790edefb0ed9c1e5
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-06-04 01:05:31 -0400

    add arrows symbol on pagination buttons

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -127,12 +127,12 @@ export default function UserList() {
           </span>
         </div>
 
-        <div className="flex justify-between my-2 xs:mt-0">
+        <div className="flex w-3/5 justify-between my-2 xs:mt-0">
           <button className="items-center px-3 h-8 text-sm font-medium text-white bg-gray-800 rounded-s hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-            Prev
+            &larr; Prev
           </button>
           <button className="items-center px-3 h-8 text-sm font-medium text-white bg-gray-800 border-0 border-s border-gray-700 rounded-e hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-            Next
+            Next &rarr;
           </button>
         </div>
       </div>
