2a2f580a15c0a405c63c32393631ebc85902e248
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-06-05 01:50:54 -0400

    ..

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -7,7 +7,7 @@ import { toast } from "react-toastify";
 export default function UserList() {
   const [userlist, setUserlist] = useState([]);
   const [pagenumber, setPagenumber] = useState(1);
-  const [userperpage, getUserperpage] = useState(0);
+  const [userperpage, setUserperpage] = useState(0);
   const [totalUsers, setTotalUsers] = useState(0);
 
   const handlePrevPage = () => {
@@ -67,12 +67,10 @@ export default function UserList() {
           }
         );
         const data = await response.json();
-        console.log(data, "uuuserdata");
-        const usersperpage = data.usersPerPage;
 
         if (response.ok) {
           setUserlist(data.paginatedUsers);
-          getUserperpage(data.usersPerPage);
+          setUserperpage(data.usersPerPage);
           setTotalUsers(data.totalEntries);
         } else {
           console.error("Failed to fetch user list");
