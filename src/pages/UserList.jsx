e2e8045e7066971502cb04a6f028480268a50e01
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-06-09 12:22:31 -0400

    fix pagination buttons position

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -102,24 +102,26 @@ export default function UserList() {
   };
 
   return (
-    <div className="min-h-dvh h-auto flex flex-col justify-items-center justify-between bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
-      <h1 className="m-10 text-center text-xl">USERS</h1>
-      <div className="flex justify-self-center mx-auto">
-        {listView ? (
-          <>
-            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-              {/* <div className="md:w-4/5 flex flex-row flex-wrap justify-center mx-auto"> */}
-              {userlist.map((user, id) => (
-                <div key={id} className="p-2">
-                  <User
-                    key={user.user_id}
-                    user={user.user_name}
-                    userId={user.user_id}
-                    userEmail={user.user_email}
-                    removeUser={handleRemove}
-                  />
-                </div>
-              ))}
+    <>
+      {listView ? (
+        <>
+          <div className="min-h-dvh h-auto flex flex-col justify-items-start justify-between bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
+            <h1 className="m-10 text-center text-xl">USERS</h1>
+            <div className="flex  justify-start mx-auto">
+              <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+                {/* <div className="md:w-4/5 flex flex-row flex-wrap justify-center mx-auto"> */}
+                {userlist.map((user, id) => (
+                  <div key={id} className="p-2">
+                    <User
+                      key={user.user_id}
+                      user={user.user_name}
+                      userId={user.user_id}
+                      userEmail={user.user_email}
+                      removeUser={handleRemove}
+                    />
+                  </div>
+                ))}
+              </div>
             </div>
             <div className="flex flex-col items-center">
               <div>
@@ -161,18 +163,62 @@ export default function UserList() {
                 </button>
               </div>
             </div>
-            ;
-          </>
-        ) : (
-          <>
-            <UserListView
-              users={userlist}
-              totalusers={totalUsers}
-              removeUser={handleRemove}
-            />
-          </>
-        )}
-      </div>
-    </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="min-h-dvh h-auto flex flex-col justify-items-center justify-between bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
+            <h1 className="m-10 text-center text-xl">USERS</h1>
+            <div className="flex justify-self-center mx-auto">
+              <UserListView
+                users={userlist}
+                totalusers={totalUsers}
+                removeUser={handleRemove}
+              />
+            </div>
+            <div className="flex flex-col items-center">
+              <div>
+                <span className="text-sm text-gray-950 dark:text-gray-400">
+                  Showing{" "}
+                  <span className="font-semibold text-gray-900 dark:text-white">
+                    {pagenumber * userperpage - (userperpage - 1)}
+                  </span>{" "}
+                  {userlist.length !== 1 ? " to " : null}
+                  {userlist.length !== 1 ? (
+                    <span className="font-semibold text-gray-900 dark:text-white">
+                      {(pagenumber - 1) * userperpage + userlist.length}
+                    </span>
+                  ) : null}
+                  <span className="font-semibold text-gray-900 dark:text-white">
+                    {" of "} {totalUserscount}
+                  </span>{" "}
+                  Total Users
+                </span>
+              </div>
+              <button onClick={handleSwitchView}> List View </button>
+
+              <div className="flex w-3/5 justify-between my-2  float-end">
+                <button
+                  disabled={pagenumber === 1}
+                  onClick={handlePrevPage}
+                  className="items-center px-4 h-8 text-sm font-medium text-white disabled:cursor-not-allowed disabled:bg-gray-300 bg-gray-800 rounded-s border  disabled:hover:bg-gray-300 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  &larr; Prev
+                </button>
+                <button
+                  disabled={
+                    pagenumber === Math.ceil(totalUserscount / userperpage)
+                  }
+                  onClick={handleNextPage}
+                  className="items-center px-4 h-8 text-sm font-medium text-white  disabled:cursor-not-allowed disabled:bg-gray-300 disabled:hover:bg-gray-300 bg-gray-800  rounded-e border border-gray-700  hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  Next &rarr;
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 }
