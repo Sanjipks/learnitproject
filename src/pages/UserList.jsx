3644e8addbf3090c662c1881bc0b870fd693f049
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-06-14 10:18:19 -0400

    fix minor display message

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -5,6 +5,8 @@ import User from "../components/User";
 import { toast } from "react-toastify";
 import { useLogin } from "../context/LoginContext";
 import UserListView from "../components/UserListView";
+import { useNavigate } from "react-router-dom";
+import Login from "./Login";
 
 export default function UserList() {
   const [userlist, setUserlist] = useState([]);
@@ -15,6 +17,11 @@ export default function UserList() {
   const [totalUserscount, setTotalUserscount] = useState(0);
   const [totalUsers, setTotalUsers] = useState([]);
   const [listView, setListView] = useState(false);
+
+  const loginInfo = useLogin();
+  const loginState = loginInfo.loginState;
+  const userRole = loginInfo.userRole;
+  const navigate = useNavigate();
 
   const startIndex = (pagenumberlist - 1) * userperpageListView;
   const endIndex = startIndex + userperpageListView;
@@ -128,136 +135,146 @@ export default function UserList() {
 
   return (
     <>
-      {listView ? (
+      {userRole === "admin" && loginState === "true" ? (
         <>
-          <div className="min-h-dvh h-auto flex flex-col justify-items-start justify-between bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
-            <h1 className="m-10 text-center text-xl">USERS</h1>
-            <div className="flex  justify-start mx-auto">
-              <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-                {/* <div className="md:w-4/5 flex flex-row flex-wrap justify-center mx-auto"> */}
-                {userlist.map((user, id) => (
-                  <div key={id} className="p-2">
-                    <User
-                      key={user.user_id}
-                      user={user.user_name}
-                      userId={user.user_id}
-                      userEmail={user.user_email}
-                      removeUser={handleRemove}
-                    />
+          {listView ? (
+            <>
+              <div className="min-h-dvh h-auto flex flex-col justify-items-start justify-between bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
+                <h1 className="m-10 text-center text-xl">USERS</h1>
+                <div className="flex  justify-start mx-auto">
+                  <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+                    {/* <div className="md:w-4/5 flex flex-row flex-wrap justify-center mx-auto"> */}
+                    {userlist.map((user, id) => (
+                      <div key={id} className="p-2">
+                        <User
+                          key={user.user_id}
+                          user={user.user_name}
+                          userId={user.user_id}
+                          userEmail={user.user_email}
+                          removeUser={handleRemove}
+                        />
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </div>
-            </div>
-            <div className="flex flex-col items-center">
-              <div>
-                {startIndex ? (
-                  <span className="text-sm text-gray-950 dark:text-gray-400">
-                    Showing{" "}
-                    <span className="font-semibold text-gray-900 dark:text-white">
-                      {pagenumber * userperpage - (userperpage - 1)}
-                    </span>{" "}
-                    {userlist.length !== 1 ? " to " : null}
-                    {userlist.length !== 1 ? (
-                      <span className="font-semibold text-gray-900 dark:text-white">
-                        {(pagenumber - 1) * userperpage + userlist.length}
+                </div>
+                <div className="flex flex-col items-center">
+                  <div>
+                    {userlist ? (
+                      <span className="text-sm text-gray-950 dark:text-gray-400">
+                        Showing{" "}
+                        <span className="font-semibold text-gray-900 dark:text-white">
+                          {pagenumber * userperpage - (userperpage - 1)}
+                        </span>{" "}
+                        {userlist.length !== 1 ? " to " : null}
+                        {userlist.length !== 1 ? (
+                          <span className="font-semibold text-gray-900 dark:text-white">
+                            {(pagenumber - 1) * userperpage + userlist.length}
+                          </span>
+                        ) : null}
+                        <span className="font-semibold text-gray-900 dark:text-white">
+                          {" of "} {totalUserscount}
+                        </span>{" "}
+                        Total Users
                       </span>
-                    ) : null}
-                    <span className="font-semibold text-gray-900 dark:text-white">
-                      {" of "} {totalUserscount}
-                    </span>{" "}
-                    Total Users
-                  </span>
-                ) : (
-                  "NO USERS TO DISPLAY"
-                )}
-              </div>
-              <button onClick={handleSwitchView}> Switch List View </button>
+                    ) : (
+                      "NO USERS TO DISPLAY"
+                    )}
+                  </div>
+                  <button onClick={handleSwitchView}> Switch List View </button>
 
-              <div className="flex w-3/5 justify-between my-2  float-end">
-                <button
-                  disabled={pagenumber === 1}
-                  onClick={handlePrevPage}
-                  className="items-center px-4 h-8 text-sm font-medium text-white disabled:cursor-not-allowed disabled:bg-gray-300 bg-gray-800 rounded-s border  disabled:hover:bg-gray-300 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  &larr; Prev
-                </button>
-                <button
-                  disabled={
-                    pagenumber === Math.ceil(totalUserscount / userperpage) ||
-                    startIndex === 0
-                  }
-                  onClick={handleNextPage}
-                  className="items-center px-4 h-8 text-sm font-medium text-white  disabled:cursor-not-allowed disabled:bg-gray-300 disabled:hover:bg-gray-300 bg-gray-800  rounded-e border border-gray-700  hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  Next &rarr;
-                </button>
+                  <div className="flex w-3/5 justify-between my-2  float-end">
+                    <button
+                      disabled={pagenumber === 1}
+                      onClick={handlePrevPage}
+                      className="items-center px-4 h-8 text-sm font-medium text-white disabled:cursor-not-allowed disabled:bg-gray-300 bg-gray-800 rounded-s border  disabled:hover:bg-gray-300 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    >
+                      &larr; Prev
+                    </button>
+                    <button
+                      disabled={
+                        pagenumber ===
+                          Math.ceil(totalUserscount / userperpage) ||
+                        startIndex === 0
+                      }
+                      onClick={handleNextPage}
+                      className="items-center px-4 h-8 text-sm font-medium text-white  disabled:cursor-not-allowed disabled:bg-gray-300 disabled:hover:bg-gray-300 bg-gray-800  rounded-e border border-gray-700  hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    >
+                      Next &rarr;
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </>
+          ) : (
+            <>
+              <div className="min-h-dvh h-auto flex flex-col justify-items-center justify-between bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
+                <h1 className="m-10 text-center text-xl">USERS</h1>
+                <div className="flex justify-self-center mx-auto">
+                  <UserListView
+                    totalusers={totalUsers}
+                    removeUser={handleRemove}
+                    handlesetuserperpagelistview={handleuserperpage}
+                    paginatedUsers={paginatedUserslistview}
+                    usernumber={userperpageListView}
+                  />
+                </div>
+                <div className="flex flex-col items-center">
+                  <div>
+                    {userlist.length !== 0 ? (
+                      <span className="text-sm text-gray-950 dark:text-gray-400">
+                        Showing{" "}
+                        <span className="font-semibold text-gray-900 dark:text-white">
+                          {startIndex + 1}
+                        </span>{" "}
+                        {startIndex !== null ? " to " : null}
+                        {endIndex !== null ? (
+                          <span className="font-semibold text-gray-900 dark:text-white">
+                            {totalUserscount < endIndex
+                              ? totalUserscount
+                              : endIndex}
+                          </span>
+                        ) : null}
+                        <span className="font-semibold text-gray-900 dark:text-white">
+                          {" of "} {totalUserscount}
+                        </span>{" "}
+                        Total Users
+                      </span>
+                    ) : (
+                      "NO USERS TO DISPLAY"
+                    )}
+                  </div>
+                  <button onClick={handleSwitchView}>
+                    {" "}
+                    Switch to Card View{" "}
+                  </button>
+
+                  <div className="flex w-3/5 justify-between my-2  float-end">
+                    <button
+                      disabled={pagenumberlist === 1}
+                      onClick={handlePrevPageList}
+                      className="items-center px-4 h-8 text-sm font-medium text-white disabled:cursor-not-allowed disabled:bg-gray-300 bg-gray-800 rounded-s border  disabled:hover:bg-gray-300 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    >
+                      &larr; Prev
+                    </button>
+                    <button
+                      disabled={
+                        pagenumberlist ===
+                          Math.ceil(totalUserscount / userperpageListView) ||
+                        userlist.length === 0
+                      }
+                      onClick={handleNextPageList}
+                      className="items-center px-4 h-8 text-sm font-medium text-white  disabled:cursor-not-allowed disabled:bg-gray-300 disabled:hover:bg-gray-300 bg-gray-800  rounded-e border border-gray-700  hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    >
+                      Next &rarr;
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
         </>
       ) : (
-        <>
-          <div className="min-h-dvh h-auto flex flex-col justify-items-center justify-between bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
-            <h1 className="m-10 text-center text-xl">USERS</h1>
-            <div className="flex justify-self-center mx-auto">
-              <UserListView
-                totalusers={totalUsers}
-                removeUser={handleRemove}
-                handlesetuserperpagelistview={handleuserperpage}
-                paginatedUsers={paginatedUserslistview}
-                usernumber={userperpageListView}
-              />
-            </div>
-            <div className="flex flex-col items-center">
-              <div>
-                {startIndex ? (
-                  <span className="text-sm text-gray-950 dark:text-gray-400">
-                    Showing{" "}
-                    <span className="font-semibold text-gray-900 dark:text-white">
-                      {startIndex + 1}
-                    </span>{" "}
-                    {startIndex !== null ? " to " : null}
-                    {endIndex !== null ? (
-                      <span className="font-semibold text-gray-900 dark:text-white">
-                        {totalUserscount < endIndex
-                          ? totalUserscount
-                          : endIndex}
-                      </span>
-                    ) : null}
-                    <span className="font-semibold text-gray-900 dark:text-white">
-                      {" of "} {totalUserscount}
-                    </span>{" "}
-                    Total Users
-                  </span>
-                ) : (
-                  "NO USERS TO DISPLAY"
-                )}
-              </div>
-              <button onClick={handleSwitchView}> Switch to Card View </button>
-
-              <div className="flex w-3/5 justify-between my-2  float-end">
-                <button
-                  disabled={pagenumberlist === 1}
-                  onClick={handlePrevPageList}
-                  className="items-center px-4 h-8 text-sm font-medium text-white disabled:cursor-not-allowed disabled:bg-gray-300 bg-gray-800 rounded-s border  disabled:hover:bg-gray-300 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  &larr; Prev
-                </button>
-                <button
-                  disabled={
-                    pagenumberlist ===
-                      Math.ceil(totalUserscount / userperpageListView) ||
-                    startIndex === 0
-                  }
-                  onClick={handleNextPageList}
-                  className="items-center px-4 h-8 text-sm font-medium text-white  disabled:cursor-not-allowed disabled:bg-gray-300 disabled:hover:bg-gray-300 bg-gray-800  rounded-e border border-gray-700  hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  Next &rarr;
-                </button>
-              </div>
-            </div>
-          </div>
-        </>
+        <Login />
       )}
     </>
   );
