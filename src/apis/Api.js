0630af637a15ffe961f938e8de561d39f1c4fabc
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-06-20 01:56:28 -0400

    manage code

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -19,3 +19,14 @@ export const getUsers = async (loggedinUserRole, pagenumber) => {
   const data = await getUsers.json();
   return data;
 };
+
+export const getServices = async (pagenumber) => {
+  const res = await fetch(`http://localhost:3000/services/page=${pagenumber}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
+  return data;
+};
