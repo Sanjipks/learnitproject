274143c2ca89966b24398001ba442a142028f2a2
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-06-22 11:42:50 -0400

    create utility folder for ulility function

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,18 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { bufferToBase64 } from "../utility/BufferToBase64";
 
 const Service = (props) => {
   const { service, serviceId, serviceLogo } = props;
   const [image, setImage] = useState(null);
-
-  const bufferToBase64 = (buffer) => {
-    let binary = "";
-    const bytes = new Uint8Array(buffer);
-    const len = bytes.byteLength;
-    for (let i = 0; i < len; i++) {
-      binary += String.fromCharCode(bytes[i]);
-    }
-    return window.btoa(binary);
-  };
 
   useEffect(() => {
     if (serviceLogo && serviceLogo.data) {
