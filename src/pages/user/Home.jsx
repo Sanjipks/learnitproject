8464b5482ee3df7f375fca5ab6f69df6e07d7ae6
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-06-23 23:58:41 -0400

    fix userprofile page

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -22,18 +22,25 @@ const Home = () => {
   return (
     <>
       {loginState === "true" && userRole === "user" ? (
-        <div className="md:h-dvh sm:h-auto sm:py-6  dark:bg-gray-600 dark:border-gray-800 bg-gray-200 border-gray-200 ">
-          <section className="flex justify-start bg-gray-300 dark:bg-gray-600">
-            <p className=" text-xl p-4">Hello, {userName}</p>
-          </section>
-          <div className=" flex justify-center px-6 mx-auto lg:pt-6 ">
-            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Welcome
-                </h1>
-                <UserProfile username={userName} />
-              </div>
+        <div className="h-dvh dark:bg-gray-600 dark:border-gray-800 bg-gray-200 border-gray-200 ">
+          <div className="max-w-screen-xl w-full flex flex-col mx-auto">
+            <div className="flex justify-start bg-gray-300 dark:bg-gray-600">
+              <p className=" text-xl p-4">Hello, {userName}</p>
+            </div>
+            <div className="flex flex-col md:flex-row sm:flex-col-reverse">
+              <section className="w-full ">Hello</section>
+              <section className="w-full">
+                <div className="flex justify-center w-full md:h-auto px-6 mx-auto lg:pt-6 ">
+                  <div className="w-full bg-white rounded-lg shadow dark:border xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                      <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Welcome
+                      </h1>
+                      <UserProfile username={userName} />
+                    </div>
+                  </div>
+                </div>
+              </section>
             </div>
           </div>
         </div>
