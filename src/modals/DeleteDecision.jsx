d2c34fbb0086792c1cdd20b7de9101d2ddd1b0a9
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-07-06 19:43:41 -0400

    ..

--- src/modals/DeleteDecision.jsx
+++ src/modals/DeleteDecision.jsx
@@ -5,7 +5,7 @@ const DeleteDecision = () => {
     <>
       <div
         id="popup-modal"
-        tabindex="-1"
+        tabIndex="-1"
         className={`hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
       >
         <div className="relative p-4 w-full max-w-md max-h-full">
@@ -24,9 +24,9 @@ const DeleteDecision = () => {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
                 />
               </svg>
@@ -42,9 +42,9 @@ const DeleteDecision = () => {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                 />
               </svg>
