ceed8243d7e25e85a374249f9f35e830dd2a9d75
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-07-07 00:48:33 -0400

    ..

--- src/modals/DeleteDecision.jsx
+++ src/modals/DeleteDecision.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 const DeleteDecision = (props) => {
   const { handlePopAction } = props;
@@ -15,7 +15,7 @@ const DeleteDecision = (props) => {
       <div
         id="popup-modal"
         tabIndex="-1"
-        className=" overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+        className="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
       >
         <div className="relative p-4 w-full max-w-md max-h-full">
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -59,7 +59,7 @@ const DeleteDecision = (props) => {
                 />
               </svg>
               <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                Are you sure you want to delete this product?
+                Are you sure you want to delete this user?
               </h3>
               <button
                 onClick={confirmDelete}
