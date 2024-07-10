e14c5d5b6aeae6da4522da48ed1576b1ae0cf8fe
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-07-10 12:42:40 -0400

    ..

--- src/pages/common/Login.jsx
+++ src/pages/common/Login.jsx
@@ -63,9 +63,9 @@ export default function Login() {
   };
 
   return (
-    <div className=" dark:bg-gray-600 dark:border-gray-800 bg-gray-200 border-gray-200 sm:pb-4 xm:pb-4 md:-mt-32">
-      <section className=" bg-gray-300 dark:bg-gray-600">
-        <div className=" flex flex-col items-center justify-center md:h-dvh px-6 mx-auto lg:pt-6 ">
+    <div className="flex justify-center items-center dark:bg-gray-600 dark:border-gray-800 bg-gray-200 border-gray-200 sm:pb-4 xm:pb-4">
+      <section className="h-auto md:my-24 bg-gray-300 dark:bg-gray-600 max-w-screen-xl w-full m-auto">
+        <div className=" flex flex-col items-center justify-center md:h-dvh px-6 lg:pt-6 ">
           <a
             href="/"
             className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
