e19e36788b8b18e0cc13237ad9f35425ad2af807
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-07-11 01:12:32 -0400

    some css work

--- src/pages/common/ForgetPassword.jsx
+++ src/pages/common/ForgetPassword.jsx
@@ -32,8 +32,8 @@ const ForgetPassword = () => {
   };
 
   return (
-    <div className="md:h-dvh dark:bg-gray-600 dark:border-gray-800 bg-gray-200 border-gray-200 sm:py-16 lg:-my-16">
-      <section className=" bg-gray-300 dark:bg-gray-600">
+    <div className="min-h-screen justify-center items-center dark:bg-gray-600 dark:border-gray-800 bg-gray-200 border-gray-200 sm:pb-4 xm:pb-4">
+      <div className=" bg-gray-300 dark:bg-gray-600 max-w-screen-xl m-auto pt-16">
         <div className=" flex flex-col items-center justify-center md:h-dvh px-6 mx-auto lg:pt-6 ">
           <p className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
             Reset your Password
@@ -69,7 +69,7 @@ const ForgetPassword = () => {
             </div>
           </div>
         </div>
-      </section>
+      </div>
     </div>
   );
 };
