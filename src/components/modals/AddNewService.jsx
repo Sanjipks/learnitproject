9275a3bbe6f27b800337fd06f6a5fa6fb524680c
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-07-19 12:57:45 -0400

    add addService compo

---
+++ src/components/modals/AddNewService.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+const AddNewService = () => {
+  return (
+    <>
+      <div className="z-50 absolute p-4 w-full max-w-md max-h-full mt-16">
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              Add Service
+            </h3>
+          </div>
+
+          <form className="p-4 md:p-5">
+            <div className="grid gap-4 mb-4 grid-cols-2">
+              <div className="col-span-2">
+                <label
+                  htmlFor="service-name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Add Service Name
+                </label>
+                <input
+                  type="text"
+                  name="service-name"
+                  id="service-name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="type service name"
+                  required=""
+                />
+              </div>
+              <div className="col-span-2">
+                <label
+                  htmlFor="service-code"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Email
+                </label>
+                <input
+                  type="text"
+                  name="service-code"
+                  id="service-code"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="type service code"
+                  required=""
+                />
+              </div>
+              <div className="col-span-2">
+                <label
+                  htmlFor="service-image"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Image
+                </label>
+                <input
+                  type="image"
+                  name="service-image"
+                  id="service-image"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="add image"
+                  required=""
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddNewService;
--- src/pages/common/Services.jsx
+++ src/pages/common/Services.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Service from "../../components/ServiceCard";
 import { getServices } from "../../apis/Api";
+import AddNewService from "../../components/modals/AddNewService";
 
 const Services = () => {
   const [servicesList, setServicesList] = useState([]);
   const [servicesPerpage, setServicesPerpage] = useState(0);
   const [totalServicesCount, setTotalServicesCount] = useState(0);
   const [pagenumber, setPagenumber] = useState(1);
+  const [openAddForm, setOpenAddForm] = useState(false);
 
   const handlePrevPage = () => {
     if (pagenumber > 1) {
@@ -20,6 +22,10 @@ const Services = () => {
     }
   };
 
+  const handleOpenAddService = () => {
+    setOpenAddForm((prev) => !prev);
+  };
+
   useEffect(() => {
     getServices(pagenumber).then((data) => {
       setServicesList(data.paginatedServices);
@@ -29,81 +35,88 @@ const Services = () => {
   }, [pagenumber]);
 
   return (
-    <div className="min-h-screen  mx-auto h-auto flex flex-col justify-items-center justify-center bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
-      <div className="md:my-20 max-w-screen-xl flex justify-center w-full mx-auto">
-        <div className="w-full flex mx-auto justify-center bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800">
-          <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white p-4 ">
-            Services
-          </h1>
+    <>
+      <div className="min-h-screen  mx-auto h-auto flex flex-col justify-items-center justify-center bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
+        <div className="md:my-20 max-w-screen-xl flex justify-center w-full mx-auto">
+          <div className="w-full flex mx-auto justify-center bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800">
+            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white p-4 ">
+              Services
+            </h1>
+          </div>
         </div>
-      </div>
-      <div className="max-w-screen-xl flex p-4 mx-auto">
-        <button className=" flex items-center bg-white rounded-lg shadow dark:border md:mt-0 max-w-sm w-auto p-4 dark:bg-gray-800 text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white  ">
-          Create New Service
-        </button>
-      </div>
+        <div className="max-w-screen-xl flex p-4 mx-auto">
+          <button
+            onClick={handleOpenAddService}
+            className=" flex items-center bg-white rounded-lg shadow dark:border md:mt-0 max-w-sm w-auto p-4 dark:bg-gray-800 text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white  "
+          >
+            Add New Service
+          </button>
+          {openAddForm ? <AddNewService /> : <></>}
+        </div>
 
-      <div className="flex flex-col justify-evenly mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-          {/* <div className="md:w-4/5 flex flex-row flex-wrap justify-center mx-auto"> */}
-          {servicesList.map((service, id) => (
-            <div key={id} className="p-2">
-              <Service
-                key={service.service_id}
-                service={service.service_name}
-                serviceId={service.service_id}
-                serviceLogo={service.service_image}
-                // addService={handleAdd}
-              />
-            </div>
-          ))}
+        <div className="flex flex-col justify-evenly mx-auto">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+            {/* <div className="md:w-4/5 flex flex-row flex-wrap justify-center mx-auto"> */}
+            {servicesList.map((service, id) => (
+              <div key={id} className="p-2">
+                <Service
+                  key={service.service_id}
+                  service={service.service_name}
+                  serviceId={service.service_id}
+                  serviceLogo={service.service_image}
+                  // addService={handleAdd}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div>
-          {servicesList ? (
-            <span className="text-sm text-gray-950 dark:text-gray-400">
-              Showing{" "}
-              <span className="font-semibold text-gray-900 dark:text-white">
-                {pagenumber * servicesPerpage - (servicesPerpage - 1)}
-              </span>{" "}
-              {servicesList.length !== 1 ? " to " : null}
-              {servicesList.length !== 1 ? (
+        <div className="flex flex-col items-center">
+          <div>
+            {servicesList ? (
+              <span className="text-sm text-gray-950 dark:text-gray-400">
+                Showing{" "}
                 <span className="font-semibold text-gray-900 dark:text-white">
-                  {(pagenumber - 1) * servicesPerpage + servicesList.length}
-                </span>
-              ) : null}
-              <span className="font-semibold text-gray-900 dark:text-white">
-                {" of "} {totalServicesCount}
-              </span>{" "}
-              Total Services
-            </span>
-          ) : (
-            "NO USERS TO DISPLAY"
-          )}
-        </div>
+                  {pagenumber * servicesPerpage - (servicesPerpage - 1)}
+                </span>{" "}
+                {servicesList.length !== 1 ? " to " : null}
+                {servicesList.length !== 1 ? (
+                  <span className="font-semibold text-gray-900 dark:text-white">
+                    {(pagenumber - 1) * servicesPerpage + servicesList.length}
+                  </span>
+                ) : null}
+                <span className="font-semibold text-gray-900 dark:text-white">
+                  {" of "} {totalServicesCount}
+                </span>{" "}
+                Total Services
+              </span>
+            ) : (
+              "NO USERS TO DISPLAY"
+            )}
+          </div>
 
-        <div className="flex max-w-screen-xl w-full justify-between my-2  float-end">
-          <button
-            disabled={pagenumber === 1}
-            onClick={handlePrevPage}
-            className="items-center px-4 h-8 text-sm font-medium text-white disabled:cursor-not-allowed disabled:bg-gray-300 bg-gray-800 rounded-s border  disabled:hover:bg-gray-300 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            &larr; Prev
-          </button>
-          <button
-            disabled={
-              pagenumber === Math.ceil(totalServicesCount / servicesPerpage) ||
-              servicesList.length === 0
-            }
-            onClick={handleNextPage}
-            className="items-center px-4 h-8 text-sm font-medium text-white  disabled:cursor-not-allowed disabled:bg-gray-300 disabled:hover:bg-gray-300 bg-gray-800  rounded-e border border-gray-700  hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            Next &rarr;
-          </button>
+          <div className="flex max-w-screen-xl w-full justify-between my-2  float-end">
+            <button
+              disabled={pagenumber === 1}
+              onClick={handlePrevPage}
+              className="items-center px-4 h-8 text-sm font-medium text-white disabled:cursor-not-allowed disabled:bg-gray-300 bg-gray-800 rounded-s border  disabled:hover:bg-gray-300 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              &larr; Prev
+            </button>
+            <button
+              disabled={
+                pagenumber ===
+                  Math.ceil(totalServicesCount / servicesPerpage) ||
+                servicesList.length === 0
+              }
+              onClick={handleNextPage}
+              className="items-center px-4 h-8 text-sm font-medium text-white  disabled:cursor-not-allowed disabled:bg-gray-300 disabled:hover:bg-gray-300 bg-gray-800  rounded-e border border-gray-700  hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Next &rarr;
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
