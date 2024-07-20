5f3077ef305564b6cade915b3d1b417774d127a3
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-07-20 00:44:37 -0400

    minor typo change

--- src/components/modals/AddNewService.jsx
+++ src/components/modals/AddNewService.jsx
@@ -34,7 +34,7 @@ const AddNewService = () => {
                   htmlFor="service-code"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Email
+                  Service Code
                 </label>
                 <input
                   type="text"
@@ -53,7 +53,7 @@ const AddNewService = () => {
                   Image
                 </label>
                 <input
-                  type="image"
+                  type="file"
                   name="service-image"
                   id="service-image"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
