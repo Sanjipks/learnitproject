3924b204222e5255883a4f379c124e9dfbd151f0
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-07-22 11:15:56 -0400

    add some logic to handle event

--- src/components/modals/AddNewService.jsx
+++ src/components/modals/AddNewService.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddNewService = () => {
+  const [inputs, setInputs] = useState({
+    servicename: "",
+    servicecode: null,
+    serviceImage: null,
+  });
+
+  const handleInput = (event) => {
+    setInputs({ ...inputs, [event.target.name]: event.target.value });
+  };
+
+  const handleImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setInputs({
+        ...inputs,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+  const handleSubmit = () => {
+    "todo";
+  };
   return (
     <>
       <div className="z-50 absolute p-4 w-full max-w-md max-h-full mt-16">
@@ -24,6 +46,7 @@ const AddNewService = () => {
                   type="text"
                   name="service-name"
                   id="service-name"
+                  onChange={handleInput}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="type service name"
                   required=""
@@ -40,6 +63,7 @@ const AddNewService = () => {
                   type="text"
                   name="service-code"
                   id="service-code"
+                  onChange={handleInput}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="type service code"
                   required=""
@@ -56,6 +80,7 @@ const AddNewService = () => {
                   type="file"
                   name="service-image"
                   id="service-image"
+                  onChange={handleImageChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="add image"
                   required=""
@@ -64,6 +89,7 @@ const AddNewService = () => {
             </div>
             <button
               type="submit"
+              onSubmit={handleSubmit}
               className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Submit
