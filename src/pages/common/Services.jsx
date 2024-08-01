1b82312e89dbb353debabebf69bdf3e78781e4a9
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-08-01 01:55:34 -0400

    fix services page view

--- src/pages/common/Services.jsx
+++ src/pages/common/Services.jsx
@@ -94,46 +94,48 @@ const Services = () => {
       {pop ? (
         <DeleteDecision handlePopAction={handlePop} type="service" />
       ) : null}
-      <div className="min-h-screen  mx-auto h-auto flex flex-col justify-items-center justify-center bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
-        <div className="md:my-20 max-w-screen-xl flex justify-center w-full mx-auto">
-          <div className="w-full flex mx-auto justify-center bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800">
-            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white p-4 ">
-              Services
-            </h1>
+      <div className="min-h-screen  mx-auto h-auto flex flex-col justify-items-center justify-between bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
+        <div className="mt-28">
+          <div className=" max-w-screen-xl flex w-full mx-auto">
+            <div className="w-full flex mx-auto justify-center bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800">
+              <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white p-4 ">
+                Services
+              </h1>
+            </div>
           </div>
-        </div>
-        <div className="max-w-screen-xl flex p-4 mx-auto">
-          <button
-            onClick={handleOpenAddService}
-            className=" flex items-center bg-white rounded-lg shadow dark:border md:mt-0 max-w-sm w-auto p-4 dark:bg-gray-800 text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white  "
-          >
-            Add New Service
-          </button>
-          {openAddForm ? (
-            <AddNewService handleformview={handleFormView} />
-          ) : (
-            <></>
-          )}
+          <div className="max-w-screen-xl flex justify-end px-2  mx-auto mt-12">
+            <button
+              onClick={handleOpenAddService}
+              className=" flex items-center bg-white rounded-lg shadow dark:border md:mt-0 max-w-sm w-auto p-4 dark:bg-gray-800 text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white  "
+            >
+              Add New Service +
+            </button>
+            {openAddForm ? (
+              <AddNewService handleformview={handleFormView} />
+            ) : (
+              <></>
+            )}
+          </div>
+          <div className="flex justify-center mt-2">
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+              {/* <div className="md:w-4/5 flex flex-row flex-wrap justify-center mx-auto"> */}
+              {servicesList.map((service, id) => (
+                <div key={id} className="p-2">
+                  <Service
+                    key={service.service_id}
+                    service={service.service_name}
+                    serviceId={service.service_id}
+                    serviceLogo={service.service_image}
+                    deleteService={handleDelete}
+                    // addService={handleAdd}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
 
-        <div className="flex flex-col justify-evenly mx-auto">
-          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-            {/* <div className="md:w-4/5 flex flex-row flex-wrap justify-center mx-auto"> */}
-            {servicesList.map((service, id) => (
-              <div key={id} className="p-2">
-                <Service
-                  key={service.service_id}
-                  service={service.service_name}
-                  serviceId={service.service_id}
-                  serviceLogo={service.service_image}
-                  deleteService={handleDelete}
-                  // addService={handleAdd}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center ">
           <div>
             {servicesList ? (
               <span className="text-sm text-gray-950 dark:text-gray-400">
