a63d425c4dc17a700f6dcd8d6f6d3e922ddb1859
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-08-17 01:43:34 -0400

    ..

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,14 +4,7 @@ const Cart = (props) => {
   const { cartItems } = props;
   return (
     <div>
-      <h3>
-        Cart Items: <span>{cartItems.length}</span>
-      </h3>
-      {/* <ul>
-        {cartItems.map((item, index) => (
-          <li key={index}>Item ID: {item}</li>
-        ))}
-      </ul> */}
+      Cart Items: <span>{" " + cartItems.length}</span>
     </div>
   );
 };
