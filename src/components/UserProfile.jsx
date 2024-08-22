1cfbd65d8504e70194915440bd056ad53f6b9dc7
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-08-22 14:22:09 -0400

    add username and email on userInfo component

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -103,20 +103,6 @@ function UserProfile() {
             src={userImage}
             alt={`${username}` + "'s profile"}
           />
-          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-            {username}
-          </h5>
-          <span className="text-sm text-gray-500 dark:text-gray-400">
-            Visual Designer
-          </span>
-          <div className="flex mt-4 md:mt-6">
-            <a
-              href="#"
-              className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-            >
-              Message
-            </a>
-          </div>
         </div>
       </div>
     </div>
