09e95ecd6c7599ea7ef6d8b2e9befec011ec86a2
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-09-19 11:17:49 -0400

    update register page

--- src/pages/common/Login.jsx
+++ src/pages/common/Login.jsx
@@ -79,7 +79,7 @@ export default function Login() {
 
   return (
     <div className="min-h-screen h-auto flex flex-col xm:gap-4 bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
-      <div className="items-center justify-start xl:mt-80 px-2">
+      <div className="items-center justify-start md:my-40 sm:my-20 px-2">
         <div className="max-w-screen-xl w-full sm:h-auto sm:m-auto flex md:flex-row xm:flex-col-reverse">
           <section className="flex justify-center items-center w-full bg-blue-200 sm:rounded-br-xl md:rounded-br-none sm:rounded-bl-xl md:rounded-tl-xl">
             <img
