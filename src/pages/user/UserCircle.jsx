32798752de5902487c3beed35fe65e857cb79450
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-10-13 13:54:54 -0400

    add user circle

---
+++ src/pages/user/UserCircle.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UserCircle = () => {
+  return (
+    <div className="min-h-screen h-auto flex flex-col justify-items-center justify-between bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
+      <div className="md:my-20">
+        <div className="w-full flex mx-auto justify-center bg-gray-100 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800">
+          <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white p-4 ">
+            Users Circle
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCircle;
