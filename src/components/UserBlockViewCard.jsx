6d6a4e3fdcad37a63ff39197e0373541d985ee59
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-10-16 12:58:57 -0400

    change text color and hover in expaned menu item in userBlockView

--- src/components/UserBlockViewCard.jsx
+++ src/components/UserBlockViewCard.jsx
@@ -166,14 +166,14 @@ export default function User(props) {
                   className={`z-10 ${expand} absolute ml-10 text-base list-none bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}
                 >
                   <ul className="py-2" aria-labelledby="dropdownButton">
-                    <li className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                    <li className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
                       connect
                     </li>
-                    <li className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                    <li className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
                       send Message
                     </li>
 
-                    <li className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                    <li className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
                       chat
                     </li>
                   </ul>
