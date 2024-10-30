b1ac909183ee727f23ceedb718e5839acd237ec4
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-10-29 21:21:09 -0400

    ..

--- src/components/UsersCardForCircle.jsx
+++ src/components/UsersCardForCircle.jsx
@@ -106,7 +106,7 @@ export default function UsersForCircle(props) {
                     </li>
 
                     <li
-                      onClick={() => handleOpenChatbox(true)}
+                      onClick={() => handleOpenChatbox}
                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                     >
                       chat
