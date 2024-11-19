82bac1f264fcc4492f5b8d7d19a64253cb1d6db1
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-11-18 21:34:19 -0500

    add buttons of user portal

--- src/pages/user/LeanerPortal.jsx
+++ src/pages/user/LeanerPortal.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Progress() {
+  const [classes, setClasses] = useState(false);
+  const [mrProgress, setMyProgress] = useState(false);
+
+  const handleClasses = () => {
+    setClasses(true);
+  };
+  const handleMyProgress = () => {
+    setMyProgress(true);
+    setClasses(false);
+  };
   return (
     <div className="min-h-screen h-auto flex flex-col justify-items-center justify-between bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
       <div className="md:my-20">
@@ -8,6 +18,23 @@ export default function Progress() {
           <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white p-4 ">
             Learner Portal
           </h1>
+        </div>
+        <div className="flex flex-row  justify-evenly mx-auto gap-8">
+          <button
+            onClick={handleClasses}
+            className="flex py-3 w-48 justify-center my-4 border border-white rounded-lg text-lg  "
+          >
+            Classes
+          </button>
+          <button
+            onClick={handleMyProgress}
+            className="flex py-3  w-48 justify-center my-4 border border-white rounded-lg text-lg "
+          >
+            My Progress
+          </button>
+        </div>
+        <div className="flex justify-center mx-auto">
+          {classes ? <div>My Classes</div> : <div>My Progress</div>}
         </div>
       </div>
     </div>
