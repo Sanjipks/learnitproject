f0d63970b47f2761f41a23f1b7946fc8375d10a5
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-11-29 14:13:32 -0500

    add messaging component

---
+++ src/components/Messaging.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Messaging = () => {
+  return (
+    <div className="flex h-3/4 mt-40 fixed w-96 justify-center border dark:border-gray-300 mr-2 rounded-md">
+      Messaging
+    </div>
+  );
+};
+
+export default Messaging;
