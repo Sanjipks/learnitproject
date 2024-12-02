350bb9f11c95b81551a435f1b63e79da539e0d28
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-12-02 18:43:25 -0500

    fix messaging box to the top level with footer

--- src/components/Messaging.jsx
+++ src/components/Messaging.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Messaging = () => {
   return (
-    <div className="flex h-3/4 mt-40 fixed z-10 w-96 justify-center border dark:border-gray-300 ml-2 rounded-md">
-      <h1 className="border h-10 w-96 mx-auto text-center justify-center py-2 rounded-md">
+    <div className="flex h-5/6 fixed bottom-1 z-10 w-96 justify-center border border-gray-900 dark:border-gray-100 ml-1 rounded-md bg-slate-100 dark:bg-slate-700">
+      <h1 className="border h-10 w-96 mx-auto text-center justify-center py-2 rounded-md dark:text-slate-200 text-slate-900">
         {" "}
         Messaging
       </h1>
