e357724cfe5c2ae2f1de1de9fa2beb07476c41ee
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-12-02 18:47:44 -0500

    ..

--- src/components/Messaging.jsx
+++ src/components/Messaging.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Messaging = () => {
   return (
     <div className="flex h-5/6 fixed bottom-1 z-10 w-96 justify-center border border-gray-900 dark:border-gray-100 ml-1 rounded-md bg-slate-100 dark:bg-slate-700">
-      <h1 className="border h-10 w-96 mx-auto text-center justify-center py-2 rounded-md dark:text-slate-200 text-slate-900">
+      <h1 className="border border-gray-900 dark:border-gray-100 h-10 w-96 mx-auto text-center justify-center py-2 rounded-md dark:text-slate-200 text-slate-900">
         {" "}
         Messaging
       </h1>
