9c7b27fdf5f6809e674255f1414e8b3e65435c50
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-12-06 18:11:51 -0500

    fix scroll and messaging header issue

--- src/components/Messaging.jsx
+++ src/components/Messaging.jsx
@@ -10,22 +10,23 @@ const Messaging = () => {
 
   return (
     <div
-      className={` ${viewBox ? "h-5/6" : "h-10"} ${
+      className={`${viewBox ? "h-5/6" : "h-10"} ${
         closeBox && "hidden"
-      } fixed bottom-1 z-10  justify-center border border-gray-900 dark:border-gray-100 ml-1 rounded-md bg-slate-100 dark:bg-slate-700`}
+      } fixed bottom-1 z-10 flex flex-col justify-center border border-gray-900 dark:border-gray-100 ml-1 rounded-md bg-slate-100 dark:bg-slate-700`}
     >
-      <h1 className=" flex justify-between border border-gray-900 z-10 dark:border-gray-100 h-10 mx-auto text-center py-2 rounded-t-md dark:text-slate-200 text-slate-900">
+      <h1 className="flex w-full justify-between border-b border-gray-900 z-10 dark:border-gray-100 h-10 mx-auto text-center py-2 rounded-t-md dark:text-slate-200 text-slate-900">
         <button className="w-12 ml-2" onClick={handleCloseBox}>
           <CloseIcon />
         </button>
 
         <span>Messaging</span>
+
         <button className="w-12 mr-2" onClick={handleViewBox}>
           ---
         </button>
       </h1>
-
-      <div className="flex w-auto  overflow-hidden h-auto border border-t-0 border-gray-900 dark:border-gray-100 dark:text-slate-200 text-slate-900">
+      \
+      <div className="flex-grow overflow-y-auto w-full border-t-0 border-gray-900 dark:border-gray-100 dark:text-slate-200 text-slate-900">
         <UserChatlistsForMessaging />
       </div>
     </div>
--- src/components/UsersCardForMessaging.jsx
+++ src/components/UsersCardForMessaging.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { bufferToBase64 } from "../utility/BufferToBase64";
 import { useLogin } from "../context/LoginContext";
-import ChatBox from "./Chatbox";
 
 export default function UsersForMessaging(props) {
   const { loginInfo } = useLogin();
@@ -51,25 +50,25 @@ export default function UsersForMessaging(props) {
   return (
     <>
       {loggedInUserRole === "user" && (
-        <div className="w-96 mr-2 bg-gray-100 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          {connStatus === "friend" && (
-            <div
-              className="flex w-full flex-row  items-center p-1 mx-auto"
-              onClick={() => handleOpenChatbox(userId, user)}
-            >
-              <img
-                className="w-12 h-12 my-2 mr-1 rounded-full shadow-lg"
-                src={image}
-                alt="Bonnie image"
-              />
-              <div className="mb-10 text-sm font-medium text-gray-900 dark:text-white">
-                {user}
-              </div>
-              <div className="mt-6 ml-14 absolute w-80 h-10 border border-gray-900 text-sm font-medium rounded-sm  bg-gray-100 text-gray-900 ">
-                <span className="ml-2">lates Messages</span>
-              </div>
+        <div className="w-96 sticky mr-2 bg-gray-100 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          {/* {connStatus === "friend" && ( */}
+          <div
+            className="flex w-full flex-row  items-center p-1 mx-auto"
+            onClick={() => handleOpenChatbox(userId, user)}
+          >
+            <img
+              className="w-12 h-12 my-2 mr-1 rounded-full shadow-lg"
+              src={image}
+              alt="Bonnie image"
+            />
+            <div className="mb-10  text-sm font-medium text-gray-900 dark:text-white">
+              {user}
             </div>
-          )}
+            <div className="mt-6 ml-14 absolute w-80 h-10 border border-gray-900 text-sm font-medium rounded-sm  bg-gray-100 text-gray-900 ">
+              <span className=" w-80 ">lates Messages</span>
+            </div>
+          </div>
+          {/* )} */}
         </div>
       )}
     </>
