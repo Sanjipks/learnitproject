be6a6b0bfc2bd55e0160aaac3289b126dedf753c
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-12-10 00:59:48 -0500

    hiding messaging box when uselogin state is not true

--- src/components/Messaging.jsx
+++ src/components/Messaging.jsx
@@ -3,33 +3,40 @@ import CloseIcon from "../assets/icons/CloseIcon";
 import { useMessaging } from "../context/MessagingContext";
 
 import UserChatlistsForMessaging from "../pages/user/UserChatLists";
+import { useLogin } from "../context/LoginContext";
 
 const Messaging = () => {
+  const { loginInfo } = useLogin();
+  const userState = loginInfo.loginState;
   const { chatlists, closeBox, handleCloseBox, viewBox, handleViewBox } =
     useMessaging();
 
   return (
-    <div
-      className={`${viewBox ? "h-5/6" : "h-10"} ${
-        closeBox && "hidden"
-      } fixed bottom-1 z-10 flex flex-col max-w-md w-[400px] ml-1 justify-center border border-gray-900 dark:border-gray-100  rounded-md bg-slate-100 dark:bg-slate-700`}
-    >
-      <h1 className="flex max-w-md w-full justify-between border border-gray-900 z-10 dark:border-gray-100 h-10 mx-auto text-center py-2 rounded-t-md dark:text-slate-200 text-slate-900">
-        <button className="w-12 ml-2" onClick={handleCloseBox}>
-          <CloseIcon />
-        </button>
+    <>
+      {userState === "true" && (
+        <div
+          className={`${viewBox ? "h-5/6" : "h-10"} ${
+            closeBox && "hidden"
+          } fixed bottom-1 z-10 flex flex-col max-w-md w-[400px] ml-1 justify-center border border-gray-900 dark:border-gray-100  rounded-md bg-slate-100 dark:bg-slate-700`}
+        >
+          <h1 className="flex max-w-md w-full justify-between border border-gray-900 z-10 dark:border-gray-100 h-10 mx-auto text-center py-2 rounded-t-md dark:text-slate-200 text-slate-900">
+            <button className="w-12 ml-2" onClick={handleCloseBox}>
+              <CloseIcon />
+            </button>
 
-        <span>Messaging</span>
+            <span>Messaging</span>
 
-        <button className="w-12 mr-2" onClick={handleViewBox}>
-          ---
-        </button>
-      </h1>
+            <button className="w-12 mr-2" onClick={handleViewBox}>
+              ---
+            </button>
+          </h1>
 
-      <div className="flex-grow overflow-y-auto w-full border border-gray-900 dark:border-gray-100 dark:text-slate-200 text-slate-900">
-        <UserChatlistsForMessaging />
-      </div>
-    </div>
+          <div className="flex-grow overflow-y-auto w-full border border-gray-900 dark:border-gray-100 dark:text-slate-200 text-slate-900">
+            <UserChatlistsForMessaging />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
