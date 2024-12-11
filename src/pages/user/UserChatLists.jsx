16897264c3a28b98fe43ef34adfc9bb67c270d63
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-12-11 14:47:17 -0500

    fix messaging height accordingly

--- src/pages/user/UserChatLists.jsx
+++ src/pages/user/UserChatLists.jsx
@@ -59,7 +59,7 @@ const UserChatlistsForMessaging = () => {
   };
 
   return (
-    <div className="flex flex-col  bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
+    <div className="flex flex-col bg-gray-500 dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
       <div className="flex-none py-6 border-b border-gray-400">
         <button
           className="flex px-6 py-2 my-2 border border-white rounded-lg text-lg mx-auto"
@@ -76,7 +76,7 @@ const UserChatlistsForMessaging = () => {
         )}
       </div>
 
-      <div className="flex-grow-1 overflow-y-scroll">
+      <div className="flex-grow overflow-y-scroll">
         {loading ? (
           <Spinner />
         ) : (
