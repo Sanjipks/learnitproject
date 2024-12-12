1bf1c634d442d59ae0761146b20d081722387147
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-12-11 22:41:52 -0500

    place getUsers at the top

--- src/context/MessagingContext.jsx
+++ src/context/MessagingContext.jsx
@@ -1,4 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { useLogin } from "./LoginContext";
+import { getUsers } from "../apis/Api";
 
 const MessagingCotext = createContext();
 
@@ -7,12 +9,34 @@ export const useMessaging = () => {
 };
 
 export const MessagingProvider = ({ children }) => {
+  const { loginInfo } = useLogin();
+  const pagenumber = 1;
+  const loggedinUserRole = loginInfo.userRole;
+  const loggedInUserId = loginInfo.userId;
   const [viewBox, setViewBox] = useState(true);
+  const [allUsers, setAllUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [closeBox, setCloseBox] = useState(false);
   const [chatlists, setChatlists] = useState([]);
 
   useEffect(() => {
-    setChatlists(["todo"]);
+    const fetchUsers = async () => {
+      setLoading(true);
+      try {
+        const data = await getUsers(
+          loggedinUserRole,
+          pagenumber,
+          loggedInUserId
+        );
+        setAllUsers(data.allUsers);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
   }, []);
 
   const chatListsUpdate = (updates) => {
@@ -31,6 +55,8 @@ export const MessagingProvider = ({ children }) => {
   return (
     <MessagingCotext.Provider
       value={{
+        loading,
+        allUsers,
         chatlists,
         chatListsUpdate,
         closeBox,
