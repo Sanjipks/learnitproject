325fd9348e69eb02402bc1a89fd36adc81c8fd94
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-12-17 11:14:01 -0500

    add year

--- src/utility/TimestampToRE.js
+++ src/utility/TimestampToRE.js
@@ -1,13 +1,33 @@
+export const TimeStampToMonth = (timestamp) => {
+  const dateObject = new Date(timestamp);
+  const month = dateObject.toLocaleDateString("en-US", { month: "long" });
+  return month;
+};
+
+export const TimeStampToTime = (timestamp) => {
+  const dateObject = new Date(timestamp);
+
+  const options = {
+    timeZone: "America/New_York",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  };
+
+  const time = dateObject.toLocaleTimeString("en-US", options);
+  return time;
+};
 export const TimeStampToDay = (timestamp) => {
   const dateObject = new Date(timestamp);
   const day = dateObject.toLocaleDateString("en-US", { weekday: "long" });
   return day;
 };
 
-export const TimeStampToTime = (timestamp) => {
+export const TimeStampToYear = (timestamp) => {
   const dateObject = new Date(timestamp);
-  const time = dateObject.toISOString().split("T")[1].split(".")[0];
-  return time;
+  const year = dateObject.toLocaleDateString("en-US", { year: "numeric" });
+  return year;
 };
 
 export const TimeStampToDate = (timestamp) => {
