ff88b2c4c557ce800e874fbcb7c0c359d5528406
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-12-26 17:26:26 -0500

    add table for mail

--- src/components/Mailbox.jsx
+++ src/components/Mailbox.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Mailbox = () => {
   return (
-    <div>Mailbox</div>
-  )
-}
+    <div className="flex h-dvh w-full max-w-screen-xl border border-gray-50 justify-center rounded-lg mt-20">
+      <div className="flex w-full h-20 border rounded-lg border-gray-50 justify-center">
+        <h1 className="text-lg p-2">My Mails</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Mailbox
+export default Mailbox;
