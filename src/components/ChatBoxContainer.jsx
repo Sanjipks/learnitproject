71a5bd5134ed8b1e05723fb4c9a5e2c11463b3e4
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2025-01-11 19:33:24 -0500

    code organize

--- src/components/ChatBoxContainer.jsx
+++ src/components/ChatBoxContainer.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ChatBox from "./Chatbox";
+import { useChatBox } from "../context/ChatBoxContext";
 
 const ChatBoxContainer = () => {
+  const {
+    handleCloseChatBox,
+    handleMinimizeChatBox,
+    display,
+    viewChatBox,
+    selectedUserImage,
+    selectedUser,
+    selectedUserId,
+  } = useChatBox();
+
   return (
     <div>
-      <ChatBox />
+      <ChatBox
+        handleCloseChatBox={handleCloseChatBox}
+        handleMinimizeChatBox={handleMinimizeChatBox}
+        display={display}
+        viewChatBox={viewChatBox}
+        selectedUserImage={selectedUserImage}
+        selectedUser={selectedUser}
+        selectedUserId={selectedUserId}
+      />
     </div>
   );
 };
