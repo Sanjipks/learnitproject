f48474d427aa4b0be96ebc47163bfdbc292585c9
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2025-01-20 13:09:49 -0500

    minimize problem solved

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -36,8 +36,10 @@ const ChatBox = (props) => {
   return (
     <div
       className={` w-screen max-w-md  ${
-        minimizeStatus === "true" ? "h-20  fixed bottom-1  " : "flex-col h-auto"
-      } p-2 border border-gray-500 dark:border-gray-400 bg-gray-100 dark:bg-gray-600 rounded-lg shadow-md`}
+        minimizeStatus === "true"
+          ? "h-20 fixed bottom-0"
+          : "flex-col h-auto fixed bottom-0"
+      } p-2 border border-gray-500 dark:border-gray-400 bg-gray-100 dark:bg-gray-600 rounded-lg shadow-md `}
     >
       <section className="flex justify-between h-auto border-b-2 border-gray-500 dark:border-gray-100 mb-1 items-start">
         <div className="flex flex-row">
